0576ea33439070fb61d90edee21b08c75efadc19
junior-meowmeow <121653436+junior-meowmeow@users.noreply.github.com>
2023-04-26 02:36:32 +0700

    update for part4

--- script_item.js
+++ script_item.js
@@ -1,5 +1,5 @@
 // TODO #4.0: Change this IP address to EC2 instance public IP address when you are going to deploy this web application
-const backendIPAddress = "127.0.0.1:3000";
+const backendIPAddress = "3.216.102.177:3000";
 
 let itemsData;
 
